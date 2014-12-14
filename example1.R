e1d88ebe51b1194a33606d055af33877536745de
jgabry <jgabry@Jonahs-MacBook-Pro.local>
2014-12-14 17:45:07 -0500

    add examples

--- example1.R
+++ example1.R
@@ -1,3 +1,12 @@
+# In this example we sample directly from the posterior distribution of beta and
+# sigma^2 in a simple Bayesian linear regression model. Then we launch the shiny
+# app.
+
+library(shiny)
+
+
+# create fake data --------------------------------------------------------
+
 # number of observations 
 N <- 250
 
@@ -9,19 +18,20 @@ X3 <- rbinom(N, 1, 1/2)
 # model matrix (with column of 1s for intercept)
 X <- cbind(const = 1, X1, X2, X3)
 K <- ncol(X)
-# generate fake data y
+
+# generate fake outcome y
 b <- c(2, -1/4, 1/2, 5/2) # pick some coefficients
 m <- X %*% b # calculate mean
 s <- 3 # pick an sd
-y <- rnorm(N, mean =  m, sd = s)
+y <- rnorm(N, mean =  m, sd = s) # generate y values
 
-# run OLS and extract MLE estimates
+# run OLS and extract estimates
 OLS <- lm(y ~ X1 + X2 + X3)
 beta_hat <- coef(OLS)
 sigma2_hat  <- sum( (y - X %*% beta_hat)^2 ) / (N - K)
 
 
-# sample from posterior distribution under vague prior
+# sample from posterior distribution under vague prior ------------------
 v1 <- N - K
 v2 <- v1*sigma2_hat
 At <- t(chol(solve(t(X)%*%X)))
@@ -37,10 +47,16 @@ posterior_sample <- function(beta_hat, v1, v2, At) {
   draws
 }
 
-posterior <- t(replicate(n = 1e5, posterior_sample(beta_hat, v1, v2, At)))
+posterior <- t(replicate(n = 1e3, posterior_sample(beta_hat, v1, v2, At)))
 
 colnames(posterior) <- c(names(beta_hat), "sigma2")
 beta <- posterior[, names(beta_hat)]
 sigma <- sqrt(posterior[, "sigma2"])
 
 
+
+# Launch shiny app for posterior predictive check -------------------------
+
+# to use the app we need y, X, beta and sigma, all of which we have created
+# above
+shiny::runGitHub(repo = "jgabry/post_pred_shiny")
---
+++ example2_stan.R
@@ -0,0 +1,74 @@
+# In this example we use we the rstan package (the R interface to Stan) to
+# sample from the posterior distribution in a simple Bayesian linear regression
+# model. Then we launch the shiny app.
+
+library(rstan)
+library(shiny)
+
+
+# Create fake data --------------------------------------------------------
+
+# number of observations 
+N <- 250
+
+# covariates
+X1 <- rnorm(N)
+X2 <- rpois(N, 2)
+X3 <- rbinom(N, 1, 1/2)
+
+# model matrix (with column of 1s for intercept)
+X <- cbind(const = 1, X1, X2, X3)
+K <- ncol(X)
+
+# generate fake outcome y
+b <- c(2, -1/4, 1/2, 5/2) # pick some coefficients
+m <- X %*% b # calculate mean
+s <- 3 # pick an sd
+y <- rnorm(N, mean =  m, sd = s) # generate y values
+
+
+
+# Run model using Stan -------------------------------------------------------
+
+# the data we'll need
+stan_data <- list(y = y, X = X, N = N, K = K)
+
+# the Stan code
+stan_model <- "
+data {
+  # declare the data we're providing to Stan 
+  int           N ; # integer, number of observations
+  int           K ; # integer, number of columns in model matrix
+  matrix[N,K]   X ; # NxK matrix, model matrix
+  vector[N]     y ; # N-vector, outcome variable
+}
+parameters {
+  # declare the parameters in our model 
+  real<lower=0> sigma ; # real number > 0, standard deviation
+  vector[K]     beta ;  # K-vector of real numbers, regression coefficients
+}
+model {
+  # write our model 
+  beta ~ normal(0, 5) ;       # prior for beta
+  sigma ~ cauchy(0, 5) ;      # prior for sigma
+  y ~ normal(X*beta, sigma) ; # likelihood 
+}
+"
+
+# run it
+posterior <- stan(model_code = stan_model, data = stan_data, 
+                  chains = 4, iter = 500)
+
+
+# extract the posterior samples
+samples <- extract(posterior)
+beta <- samples$beta
+sigma <- samples$sigma
+
+
+# Launch shiny app for posterior predictive check -------------------------
+
+# to use the app we need y, X, beta and sigma, all of which we have created
+# above
+shiny::runGitHub(repo = "jgabry/post_pred_shiny")
+
